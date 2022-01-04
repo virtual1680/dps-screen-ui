1f0c67201d24178e7464630e38a34b114d7020b4
qinhongyang <772622542@qq.com>
2022-01-04 17:13:28 +0800

    feat:声明

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,18 @@
 /// <reference types="vite/client" />
 
+import { EChartsType } from 'echarts';
+
 declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+	import { DefineComponent } from 'vue';
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+	const component: DefineComponent<{}, {}, any>;
+	export default component;
+}
+
+import { ComponentCustomProperties } from 'vue';
+
+declare module '@vue/runtime-core' {
+	interface ComponentCustomProperties {
+		$echarts: EChartsType;
+	}
 }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -11,9 +11,44 @@ export default defineComponent({
 </script>
 
 <template>
-	<div>
+	<div class="container">
 		<screenHeader />
+		<div class="content">
+			<div class="left-container"></div>
+			<div class="right-container">
+				<div class="early-list"></div>
+				<div style="height: 245px; width: 100%; background: red"></div>
+				<div style="height: 280px; width: 100%; background: green"></div>
+				<div style="height: 310px; width: 100%; background: green"></div>
+			</div>
+		</div>
 	</div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.container {
+	height: 100%;
+}
+.content {
+	display: flex;
+	height: calc(100% - 80px);
+	.left-container {
+		width: 1165px;
+		height: 100%;
+		background: pink;
+	}
+	.right-container {
+		width: 755px;
+		height: 100%;
+		background: yellow;
+		display: flex;
+		justify-content: space-between;
+		flex-direction: column;
+		.early-list {
+			height: 80px;
+			width: 100%;
+			background: violet;
+		}
+	}
+}
+</style>
