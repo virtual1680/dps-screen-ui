68b6d00cd890912f3c0ddf5f4ae616e28c23d7d1
qinhongyang <772622542@qq.com>
2022-01-04 16:24:59 +0800

    feat:预警数据

--- src/components/earlyWarningNum/main.vue
+++ src/components/earlyWarningNum/main.vue
@@ -2,11 +2,13 @@
 	<div class="early-num-box">
 		<div class="icon"></div>
 		<div class="info">
-			<div class="title">昨日预警</div>
-			<div class="num">1420</div>
+			<div>
+				<div class="title">{{ title }}预警</div>
+				<div class="num">{{ num }}</div>
+			</div>
 			<div class="compare">
-				<div class="label">同比昨日</div>
-				<div class="value">26%</div>
+				<div class="label">{{ des }}</div>
+				<div class="value down">{{ percent }}%</div>
 			</div>
 		</div>
 	</div>
@@ -14,10 +16,26 @@
 <script lang="ts">
 // 预警数据展示
 import { defineComponent } from 'vue';
-import HelloWorld from '@components/HelloWorld.vue';
 export default defineComponent({
 	name: 'earlyWarningNum',
-	components: { HelloWorld },
+	props: {
+		title: {
+			type: String,
+			default: '昨日',
+		},
+		num: {
+			type: Number || String,
+			default: 0,
+		},
+		des: {
+			type: String,
+			default: '同比昨日',
+		},
+		percent: {
+			type: Number || String,
+			default: 0,
+		},
+	},
 	setup() {},
 });
 </script>
@@ -36,7 +54,7 @@ export default defineComponent({
 		background: green;
 	}
 	.info {
-		padding: 5px 5px 10px 10px;
+		padding: 5px 5px 8px 10px;
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
@@ -49,29 +67,43 @@ export default defineComponent({
 			line-height: 17px;
 		}
 		.num {
-			height: 24px;
-			font-size: 24px;
+			font-size: 26px;
+			font-family: ElectronicFont;
 			font-weight: bold;
 			color: #00e4ff;
-			line-height: 24px;
 		}
 		.compare {
 			display: flex;
-			align-items: center;
-			span {
-				display: inline-block;
-			}
+			align-items: flex-end;
+			align-self: flex-end;
 			.label {
 				font-size: 10px;
 				font-weight: 500;
 				color: #536f72;
-				line-height: 14px;
 			}
 			.value {
-				font-size: 14px;
-				font-weight: 500;
+				&::before {
+					content: '.';
+					color: transparent;
+					width: 10px;
+					height: 10px;
+					position: relative;
+					left: -5px;
+					font-size: 14px;
+					font-weight: 400;
+				}
+				&.down::before {
+					background: gold;
+				}
+				&.up::before {
+					background: gold;
+				}
+				padding-left: 10px;
+				top: 2px;
+				position: relative;
+				font-size: 16px;
+				font-weight: 600;
 				color: #ffffff;
-				line-height: 20px;
 			}
 		}
 	}
